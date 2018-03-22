1ba3b39c7aa147a3d1a4f2fae4c8b70aa7f61f04
Alex <alexmiddleton@outlook.com>
2018-03-22 15:38:41 -0400

    search page now visible

--- app/src/reducers/search.js
+++ app/src/reducers/search.js
@@ -1,8 +1,10 @@
 import { CHANGE_SEARCH_CRITERIA } from '../constants'
+import { merge } from 'ramda'
 
 const searchCriteria = (state = '', action) => {
   switch (action.type) {
     case CHANGE_SEARCH_CRITERIA:
+      console.dir(merge(state, action.payload))
       return action.payload
     default:
       return state
